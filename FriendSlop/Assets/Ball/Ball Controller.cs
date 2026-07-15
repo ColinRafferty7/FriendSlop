1d78fd40d8e1ef41883065723329a1c94dd6ca62
Paulo <paulopiedade2005@gmail.com>
2026-07-14 23:51:13 -0400

    ball!

--- FriendSlop/Assets/Ball/Ball Controller.cs
+++ FriendSlop/Assets/Ball/Ball Controller.cs
@@ -6,12 +6,12 @@ public class BallController : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     void Start()
     {
-        
+    
     }
 
     void Update()
     {
-        Vector3 dir = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);  
-        rb.linearVelocity = dir * 10;
+        Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal")*10, rb.linearVelocity.y , Input.GetAxisRaw("Vertical")*10);  
+        rb.linearVelocity = dir;
     }
 }
