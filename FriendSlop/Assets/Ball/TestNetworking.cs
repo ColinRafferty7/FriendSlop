b813f221450189f97d61557fb8a791732986d0c0
ColinRafferty7 <ctr6mince@gmail.com>
2026-07-18 19:45:41 -0400

    Basic host client selection

---
+++ FriendSlop/Assets/Ball/TestNetworking.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class TestNetworking : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("Host Start" + NetworkManager.Singleton.StartHost());
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("Client Start" + NetworkManager.Singleton.StartClient());
+        }
+    }
+    
+}
